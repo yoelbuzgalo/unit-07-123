e843c14ee63b08979cd2d5ed3c07abffab99f87e
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-16 10:52:12 -0400

    Activity 7.2

--- activities.py
+++ activities.py
@@ -7,10 +7,18 @@ def tuples(a_tuple):
     # Attempting to change 1 of the index
     a_tuple[2] == 5
 
+def packer():
+    return "a", 1 , "b", 2
+
 def main():
-    some_tuple = (1,2,3,4,5)
-    another_tuple = tuple("abcdef")
-    tuples(another_tuple)
+    # some_tuple = (1,2,3,4,5)
+    # another_tuple = tuple("abcdef")
+    # tuples(another_tuple)
+    packed = packer()
+    print(packed[0])
+    print(packed[1])
+    print(packed[2])
+    print(packed[3])
 
 if __name__ == "__main__":
     main()
