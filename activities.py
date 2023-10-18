95366b6494c7663686b2f4e004442f07cf46b512
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-18 10:14:32 -0400

    Activity 7.9

--- activities.py
+++ activities.py
@@ -41,6 +41,10 @@ def mutater(a_list, an_int):
 def cat(a_list, b_list):
     return a_list + b_list
 
+def extender(a_list, b_list):
+    a_list += b_list
+    return a_list
+
 def main():
     # some_tuple = (1,2,3,4,5)
     # another_tuple = tuple("abcdef")
@@ -63,7 +67,9 @@ def main():
     # print(list_1)
     l_1 = [1,2,3]
     l_2 = [4,5,6]
-    print(cat(l_1,l_2)+[7,8,9])
+    # print(cat(l_1,l_2)+[7,8,9])
+    l_3 = extender(l_1,l_2)
+    print(l_1, l_2, l_3, end="\n")
 
 if __name__ == "__main__":
     main()
