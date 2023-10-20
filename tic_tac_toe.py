7fadae2b79f58104ccf0fbffe49461e7641023f5
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-20 10:37:25 -0400

    Activity 7.25

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,8 +16,10 @@ def print_board(board):
 
 def make_move(board, current_symbol):
     try:
-        user_input_row,user_input_col = int(input("Enter row:")),int(input("Enter column:"))
-        if board[user_input_row][user_input_col] == "X" or board[user_input_row][user_input_col] == "O":
+        user_input_row , user_input_col = int(input("Enter row:")) , int(input("Enter column:"))
+        if user_input_row > 2 or user_input_col > 2:
+            raise ValueError("Invalid Index Please Try Again")
+        elif board[user_input_row][user_input_col] == "X" or board[user_input_row][user_input_col] == "O":
             raise ValueError("Invalid Location Please Try Again")
         else:
             board[user_input_row][user_input_col] = current_symbol
@@ -29,8 +31,14 @@ def make_move(board, current_symbol):
 def main():
     table = make_board()
     # print_board(table)
-    make_move(table, "X")
-    make_move(table, "O")
+    for i in range(9):
+        current_symbol = ""
+        if i % 2 == 0:
+            current_symbol = "X"
+        else:
+            current_symbol = "O"
+        make_move(table, current_symbol)
+    print("Game over!")
 
 if __name__ == "__main__":
     main()
